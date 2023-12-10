d657ac22f8aafde8c0942b6849c7d6470ee193c3
Twan van Noorloos <twan.v.n@outlook.com>
2023-12-10 16:13:59 +0000

    Added simple callback which writes to the arduino, not tested with arduino itself

--- src/arduino_communication/src/SerialCommunicationNode.cpp
+++ src/arduino_communication/src/SerialCommunicationNode.cpp
@@ -1,20 +1,45 @@
 #include "rclcpp/rclcpp.hpp"
+#include <std_msgs/msg/int16_multi_array.hpp>
 
 #include <boost/asio.hpp>
 
+using std::placeholders::_1;
+
 class SerialCommunication : public rclcpp::Node
 {
-    private:
-        
-    public:
-        SerialCommunication() : Node("serial_communication_node") {
-            RCLCPP_INFO(this->get_logger(), "New serial node started");
-        };
+private:
+    rclcpp::Subscription<std_msgs::msg::Int16MultiArray>::SharedPtr newTargetPositionSubscriber;
 
-        ~SerialCommunication() {}
+    boost::asio::io_service io;
+    boost::asio::serial_port serial;
+
+    void newTargetPositionCallback(const std_msgs::msg::Int16MultiArray &msg) {
+        RCLCPP_INFO(this->get_logger(), std::to_string(msg.data[0]).c_str());
+
+        std::string commandString = "newTargetPos " + std::to_string(msg.data[0]) + "," + std::to_string(msg.data[1]);
+
+        boost::asio::write(this->serial, boost::asio::buffer(commandString));
+    }
+
+public:
+    SerialCommunication() : Node("serial_communication_node"), io(), serial(io)
+    {
+        RCLCPP_INFO(this->get_logger(), "New serial node started");
+
+        this->newTargetPositionSubscriber = this->create_subscription<std_msgs::msg::Int16MultiArray>(
+            "/newTargetPosition",
+            10,
+            std::bind(&SerialCommunication::newTargetPositionCallback, this, _1));
+
+        this->serial.open("/dev/arduino");
+        this->serial.set_option(boost::asio::serial_port_base::baud_rate(115200));
+    };
+
+    ~SerialCommunication() {}
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<SerialCommunication>());
     rclcpp::shutdown();
