cd5d7eff43bfd2ec8740d1115db62e9cc1f99e02
Twan van Noorloos <twan.v.n@outlook.com>
2024-01-13 15:39:25 +0000

    Added callbacks for all topics from the dashboard

--- src/arduino_communication/src/SerialCommunicationNode.cpp
+++ src/arduino_communication/src/SerialCommunicationNode.cpp
@@ -1,5 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include <std_msgs/msg/int16_multi_array.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/int8.hpp>
 
 #include <boost/asio.hpp>
 
@@ -9,6 +11,9 @@ class SerialCommunication : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<std_msgs::msg::Int16MultiArray>::SharedPtr newTargetPositionSubscriber;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr newAxisPositionSubscriber;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr homeAxisSubscriber;
+    rclcpp::Subscription<std_msgs::msg::Int8>::SharedPtr dataGatheringSubscriber;
 
     boost::asio::io_service io;
     boost::asio::serial_port serial;
@@ -17,8 +22,21 @@ private:
         RCLCPP_INFO(this->get_logger(), std::to_string(msg.data[0]).c_str());
 
         std::string commandString = "newTargetPos " + std::to_string(msg.data[0]) + "," + std::to_string(msg.data[1]);
+        RCLCPP_INFO(this->get_logger(), commandString.c_str());
 
         boost::asio::write(this->serial, boost::asio::buffer(commandString));
+    }
+
+    void newAxisPositionCallback(const std_msgs::msg::String &msg) {
+        RCLCPP_INFO(this->get_logger(), msg.data.c_str());
+    }
+
+    void homeAxisCallback(const std_msgs::msg::String &msg) {
+        RCLCPP_INFO(this->get_logger(), msg.data.c_str());
+    }
+
+    void gatherDataCallback(const std_msgs::msg::Int8 &msg) {
+        RCLCPP_INFO(this->get_logger(), std::to_string(msg.data).c_str());
     }
 
 public:
@@ -27,9 +45,24 @@ public:
         RCLCPP_INFO(this->get_logger(), "New serial node started");
 
         this->newTargetPositionSubscriber = this->create_subscription<std_msgs::msg::Int16MultiArray>(
-            "/newTargetPosition",
+            "/target_position",
             10,
             std::bind(&SerialCommunication::newTargetPositionCallback, this, _1));
+
+        this->newAxisPositionSubscriber = this->create_subscription<std_msgs::msg::String>(
+            "/axis_target",
+            10,
+            std::bind(&SerialCommunication::newAxisPositionCallback, this, _1));
+
+        this->homeAxisSubscriber = this->create_subscription<std_msgs::msg::String>(
+            "/home_axis",
+            10,
+            std::bind(&SerialCommunication::homeAxisCallback, this, _1));
+
+        this->dataGatheringSubscriber = this->create_subscription<std_msgs::msg::Int8>(
+            "/data_gathering_command",
+            10,
+            std::bind(&SerialCommunication::gatherDataCallback, this, _1));
 
         this->serial.open("/dev/arduino");
         this->serial.set_option(boost::asio::serial_port_base::baud_rate(115200));
